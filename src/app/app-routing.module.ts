e9315c37d5961f15f9cfbd3d7d592f4d041e6194
Itzel Farias <fercho117_25halo@hotmail.com>
2018-10-18 13:08:49 -0500

    cambios angulares

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+// import { AuthGuard } from './auth/auth.guard';
+
+const routes: Routes = [
+  // { path: '', component: home }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  // providers: [AuthGuard]
+})
+export class AppRoutingModule {}
