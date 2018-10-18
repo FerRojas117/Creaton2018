8a27ea4a628a0c30ef39fc4f39c2cfee7e9d5c10
Itzel Farias <fercho117_25halo@hotmail.com>
2018-10-18 15:41:46 -0500

    MasterComplete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-
-// import { AuthGuard } from './auth/auth.guard';
+import { HomeComponent } from './home/home.component';
+import { GraficaComponent } from './graficas/grafica.component';
 
 const routes: Routes = [
-  // { path: '', component: home }
+   { path: '', component: HomeComponent },
+   { path: 'openchart', component: GraficaComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  // providers: [AuthGuard]
 })
 export class AppRoutingModule {}
