c72f3cd37b447b6f0c80fce660619aabced2ae39
javieramadorlag <javieramadorlag@hotmail.com>
2018-10-19 03:48:24 -0500

    totalfinal

--- src/app/serviciosPublicos/grafica.component.ts
+++ src/app/serviciosPublicos/grafica.component.ts
@@ -26,19 +26,7 @@ export class ServiciosComponent {
 
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'services'];
   dataSource = ELEMENT_DATA;
-    // Doughnut
-    public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-    public doughnutChartData:number[] = [350, 450, 100];
-    public doughnutChartType:string = 'doughnut';
-   
-    // events
-    public chartClicked(e:any):void {
-      console.log(e);
-    }
-   
-    public chartHovered(e:any):void {
-      console.log(e);
-    }
+
     /*--------------------------------------------------------------*/
     public barChartOptions:any = {
       scaleShowVerticalLines: false,
--- src/app/turismoCultura/grafica.component.ts
+++ src/app/turismoCultura/grafica.component.ts
@@ -26,19 +26,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class TurismoComponent {
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource = ELEMENT_DATA;
-    // Doughnut
-    public doughnutChartLabels:string[] = ['Presupuesto de población', 'Presupuesto de empleo y negocios', 'Presupuesto de educación', 'Presupuesto de seguridad y justicia', 'Presupuesto de salud', 'Presupuesto de servicios públicos e infraestructura', 'Presupuesto de finanzas', 'Presupuesto de turismo, relaciones exteriores y cultura', 'Presupuesto de comunicación y tecnología'];
-    public doughnutChartData:number[] = [1511193662764, 1152993487657, 334925313813, 244988874971, 122557337320, 106645504028, 27076350643, 22315591894, 11377267259];
-    public doughnutChartType:string = 'doughnut';
-   
-    // events
-    public chartClicked(e:any):void {
-      console.log(e);
-    }
-   
-    public chartHovered(e:any):void {
-      console.log(e);
-    }
+    
     /*--------------------------------------------------------------*/
     public barChartOptions:any = {
       scaleShowVerticalLines: false,
